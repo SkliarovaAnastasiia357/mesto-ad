f7431f09ae601d2731cb73f1888f2c29b5da1eda
SkliarovaAnastasiia357 <obemkapop@yandex.ru>
2026-01-12 20:21:53 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     open: true,
   },
-
-  build: {
-    outDir: "dist",
-    emptyOutDir: true, // чистка dist перед каждой сборкой
-  },
-
-  base: "/mesto-ad/",
+  base: './',
 });
