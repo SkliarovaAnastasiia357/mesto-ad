3996c4184d226549e7ccfcb565dced3bed0a6919
SkliarovaAnastasiia357 <obemkapop@yandex.ru>
2026-01-12 20:07:26 +0500

    Update card.js

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -1,9 +1,14 @@
-export const likeCard = (likeButton) => {
-  likeButton.classList.toggle("card__like-button_is-active");
-};
-
 export const deleteCard = (cardElement) => {
   cardElement.remove();
+};
+
+export const isCardLiked = (likes, userId) => {
+  return likes.some((user) => user._id === userId);
+};
+
+export const updateLikeState = (likeButton, likeCounter, likesArray) => {
+  likeButton.classList.toggle("card__like-button_is-active");
+  likeCounter.textContent = likesArray.length;
 };
 
 const getTemplate = () => {
@@ -15,28 +20,41 @@ const getTemplate = () => {
 
 export const createCardElement = (
   data,
-  { onPreviewPicture, onLikeIcon, onDeleteCard }
+  { currentUserId, onPreviewPicture, onLikeIcon, onDeleteClick }
 ) => {
   const cardElement = getTemplate();
   const likeButton = cardElement.querySelector(".card__like-button");
-  const deleteButton = cardElement.querySelector(".card__control-button_type_delete");
+  const likeCounter = cardElement.querySelector(".card__like-count");
+  const deleteButton = cardElement.querySelector(
+    ".card__control-button_type_delete"
+  );
   const cardImage = cardElement.querySelector(".card__image");
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardElement.querySelector(".card__title").textContent = data.name;
 
-  if (onLikeIcon) {
-    likeButton.addEventListener("click", () => onLikeIcon(likeButton));
+  likeCounter.textContent = data.likes.length;
+
+  if (isCardLiked(data.likes, currentUserId)) {
+    likeButton.classList.add("card__like-button_is-active");
   }
 
-  if (onDeleteCard) {
-    deleteButton.addEventListener("click", () => onDeleteCard(cardElement));
+  if (data.owner._id !== currentUserId) {
+    deleteButton.remove();
+  } else {
+    deleteButton.addEventListener("click", () =>
+      onDeleteClick(cardElement, data._id)
+    );
   }
 
-  if (onPreviewPicture) {
-    cardImage.addEventListener("click", () => onPreviewPicture({name: data.name, link: data.link}));
-  }
+  likeButton.addEventListener("click", () =>
+    onLikeIcon(likeButton, data._id, likeCounter)
+  );
+
+  cardImage.addEventListener("click", () =>
+    onPreviewPicture({ name: data.name, link: data.link })
+  );
 
   return cardElement;
 };
